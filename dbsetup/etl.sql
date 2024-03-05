52ab8a15499d6229f74c2b9324d113920bfdcd1b
Maximus Chen <104951857+HeyMaximus@users.noreply.github.com>
2024-03-05 00:10:06 -0800

    Update etl.sql

--- dbsetup/etl.sql
+++ dbsetup/etl.sql
@@ -1,6 +1,6 @@
-COPY questions FROM '/Users/owden/HackReactorPreProjects/QA-Backend/RawData/questions.csv' DELIMITER ',' CSV HEADER;
-COPY answers FROM '/Users/owden/HackReactorPreProjects/QA-Backend/RawData/answers.csv' DELIMITER ',' CSV HEADER;
-COPY answersphoto FROM '/Users/owden/HackReactorPreProjects/QA-Backend/RawData/answers_photos.csv' DELIMITER ',' CSV HEADER;
+COPY questions FROM '/QA-Backend/RawData/questions.csv' DELIMITER ',' CSV HEADER;
+COPY answers FROM '/QA-Backend/RawData/answers.csv' DELIMITER ',' CSV HEADER;
+COPY answersphoto FROM '/QA-Backend/RawData/answers_photos.csv' DELIMITER ',' CSV HEADER;
 
 SELECT setval('questions_id_seq', MAX(question_id)) FROM questions;
 SELECT setval('answers_id_seq', MAX(answer_id)) FROM answers;
